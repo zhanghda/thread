5525fc581c19518a2ad55811d27dbf7fa66891a3
zhanghda <zhanghda@mail.taiji.com.cn>
2020-05-23 10:45:15 +0800

    e

---
+++ src/main/java/com/zhanghda/test1/test1.java
@@ -0,0 +1,12 @@
+package com.zhanghda.test1;
+
+public class test1 {
+
+    public static void main(String[] args) {
+        try {
+            Thread.sleep(1000*300L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
